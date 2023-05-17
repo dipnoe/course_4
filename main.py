304e3a2443928ac35603671f8bf5500a107615ad
elena_vagner <dipnoe@gmail.com>
2023-05-17 23:27:58 +0300

    add new commands

--- main.py
+++ main.py
@@ -25,5 +25,40 @@ def user_interaction():
         json_saver.add_vacancies_to_json(vacancies)
         vacancies_json = json_saver.json_to_vacancy_class()
 
+        show_user = input("Вывести весь список? (1 - да, 2 - фильтровать)\n>>> ")
+        if show_user == '1':
+            for vacancy in vacancies_json:
+                print(vacancy)
+
+        if show_user == '2':
+            filter_word = input("""Выберите фильтр:
+1 - по минимальной зарплате
+2 - по максимальной зарплате
+3 - по городу
+4 - по опыту('Возможные значения: 1 — без опыта, 2 — от 1 года, 3 — от 3 лет, 4 — от 6 лет')\n>>> """)
+
+            input_value = input("Введите значение:\n>>> ").lower()
+            if filter_word == '1':
+                for i in json_saver.get_vacancies_by_min_salary(int(input_value)):
+                    print(i)
+            elif filter_word == '2':
+                for i in json_saver.get_vacancies_by_max_salary(int(input_value)):
+                    print(i)
+            elif filter_word == '3':
+                for i in json_saver.get_vacancies_by_area(input_value):
+                    print(i)
+            elif filter_word == '4':
+                experience = {'1': 'Без опыта',
+                              '2': 'От 1 года',
+                              '3': 'От 3 лет',
+                              '4': 'От 6 лет'
+                              }
+                for i in json_saver.get_vacancies_by_experience(experience[input_value]):
+                    print(i)
+
+            else:
+                print('Некорректное значение')
+
+
 if __name__ == "__main__":
     user_interaction()
